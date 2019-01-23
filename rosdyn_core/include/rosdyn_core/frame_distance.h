bcf6d3f8fc4e1b4760b14662f90019b794c8d0a7
ManuelBeschi <manuel.beschi@gmail.com>
2019-01-23 11:14:35 +0100

    fix frame distance orientation error sign

--- rosdyn_core/include/rosdyn_core/frame_distance.h
+++ rosdyn_core/include/rosdyn_core/frame_distance.h
@@ -6,24 +6,13 @@
 namespace rosdyn
 {
 
-  /* 
-   * Skew matrix of a vector
-   */
-  inline Eigen::Matrix3d skew(const Eigen::Vector3d& t)
-  {
-    Eigen::Matrix3d t_hat;
-    t_hat <<  0,    -t(2), t(1),
-              t(2),  0,   -t(0),
-             -t(1),  t(0), 0;
-    return t_hat;
-  }
-  
+
   /*
    * Distance between frames.
    * return a vector representing the distance of frame b w.r.t. to frame a expressed in frame w
    * the distance are defined as a vector containing:
    *   - the distance between origins (namely, origin_b_in_w - origin_a_in_w)
-   *   - the vector part of the quaternion q_ab expressed in frame w (namely R_wa * imag(q_ab))
+   *   - the double of vector part of the quaternion q_ab expressed in frame w (namely R_wa * 2*imag(q_ab) ~= angle*axis)
    */
   inline void getFrameDistance(const Eigen::Affine3d& T_wa,  const Eigen::Affine3d& T_wb,  Eigen::VectorXd& distance)
   {
@@ -39,7 +28,7 @@ namespace rosdyn
       q_ab.w()=-q_ab.w();
     }
       
-    distance.block(3, 0, 3, 1) = T_wa.linear() * q_ab.vec();
+    distance.block(3, 0, 3, 1) = -2.0*T_wa.linear() * q_ab.vec();
   }
   
   /*
@@ -47,9 +36,9 @@ namespace rosdyn
    * return a vector representing the distance of frame b w.r.t. to frame a expressed in frame w
    * the distance are defined as a vector containing:
    *   - the distance between origins (namely, origin_b_in_w - origin_a_in_w)
-   *   - the vector part of the quaternion q_ab expressed in frame w (namely R_wa * imag(q_ab))
+   *   - the double of vector part of the quaternion q_ab expressed in frame w (namely R_wa * 2*imag(q_ab) ~= angle*axis)
    * and the jacobian of the distance error defined as
-   * J=[eye(3) zeros(3, 3);zeros(3, 3) 0.5*R_wa*(eye(3)*real(q_ab)-skew(imag(q_ab)))*R_wa']
+   * J=[eye(3) zeros(3, 3);zeros(3, 3) R_wa*(eye(3)*real(q_ab)-skew(imag(q_ab)))*R_wa']
    */
   inline void getFrameDistanceJac(const Eigen::Affine3d& T_wa,  const Eigen::Affine3d& T_wb,  Eigen::VectorXd& distance,  Eigen::MatrixXd& jacobian)
   {
@@ -67,9 +56,9 @@ namespace rosdyn
     }
       
       
-    distance.block(3, 0, 3, 1) = T_wa.linear() * q_ab.vec();
+    distance.block(3, 0, 3, 1) = -2.0*T_wa.linear() * q_ab.vec();
     
-    jacobian.block(3, 3, 3, 3) = 0.5* T_wa.linear() *( q_ab.w() *Eigen::MatrixXd::Identity(3, 3) - skew(q_ab.vec()) ) *T_wa.linear().inverse(); 
+    jacobian.block(3, 3, 3, 3) = T_wa.linear() *( q_ab.w() *Eigen::MatrixXd::Identity(3, 3) - skew(q_ab.vec()) ) *T_wa.linear().inverse(); 
   }
   
   
