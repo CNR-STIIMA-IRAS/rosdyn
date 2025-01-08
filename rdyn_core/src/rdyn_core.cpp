5c053ea3594836f2bac184c7f4ee9ea97d495c8d
Manuel Beschi <manuel.beschi@unibs.it>
2025-01-08 17:26:53 +0100

    test shared lib vs interface

--- rdyn_core/src/rdyn_core.cpp
+++ rdyn_core/src/rdyn_core.cpp
@@ -1,6 +1,14 @@
-#include <rdyn_core/primitives.h>
+#include "rdyn_core/primitives.h"
+#include "rdyn_core/internal/primitives_impl.h"
 
-namespace rdyn
-{
- // nothing to do so far
-}
+namespace rdyn_core {
+
+// As the _impl.h contains the definitions, we simply include it here.
+// This makes sure that all template instantiations from _impl.h get compiled.
+
+// Ensure that all the necessary instantiations or definitions are covered in _impl.h
+
+// If you have specific non-template functions or classes that need explicit instantiation or
+// separate implementation, you can define them here.
+
+} // namespace rdyn_core
--- rdyn_core/test/rdyn_speed_test.cpp
+++ rdyn_core/test/rdyn_speed_test.cpp
@@ -1,199 +1,222 @@
-/*
-Copyright (c) 2019, Manuel Beschi CNR-STIIMA manuel.beschi@stiima.cnr.it
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the <organization> nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
+#include "rdyn_core/primitives.h"
+#include "rdyn_core/internal/primitives_impl.h"
+#include <gtest/gtest.h>
 #include <boost/filesystem.hpp>
-#include <rdyn_core/primitives.h>
 #include <rdyn_core/urdf_parser.h>
 #include <string>
 #include <vector>
 #include <thread>
+#include <chrono>
 
 #if !defined(PROJECT_SRC_DIRECTORY)
   #error "The test need that the src directory is pre-compiled. Check the CMAKE";
 #else
   constexpr const char* _PROJECT_SRC_DIRECTORY = PROJECT_SRC_DIRECTORY;
 #endif
-int main(int argc, char **argv)
-{
-  boost::filesystem::path src_path(_PROJECT_SRC_DIRECTORY);
-  boost::filesystem::path urdf_path = src_path / "test/ur10.urdf";
-  std::cout << "The config file path is : " << urdf_path << std::endl;	
 
-  std::string base_frame = "base_link";
-  std::string tool_frame = "tool0";
+namespace {
 
+class RdynCoreTest : public ::testing::Test {
+protected:
+  void SetUp() override {
+    src_path = boost::filesystem::path(_PROJECT_SRC_DIRECTORY);
+    urdf_path = src_path / "ur10.urdf";
+    base_frame = "base_link";
+    tool_frame = "tool0";
+    grav << 0, 0, -9.806;
+    chain = rdyn::createChainFromFile(urdf_path.string(), base_frame, tool_frame, grav);
+    n_joints = chain->getActiveJointsNumber();
+  }
+
+  boost::filesystem::path src_path;
+  boost::filesystem::path urdf_path;
+  std::string base_frame;
+  std::string tool_frame;
   Eigen::Vector3d grav;
-  grav << 0, 0, -9.806;
+  shared_ptr_namespace::shared_ptr<rdyn::Chain> chain;
+  unsigned int n_joints;
+};
 
-  shared_ptr_namespace::shared_ptr<rdyn::Chain> chain = rdyn::createChainFromFile(urdf_path.string(), base_frame, tool_frame, grav);
+TEST_F(RdynCoreTest, ChainCreation) {
+  ASSERT_NE(chain, nullptr);
+  ASSERT_EQ(chain->getActiveJointsNumber(), n_joints);
+}
 
-
-  unsigned int n_joints = chain->getActiveJointsNumber();
+TEST_F(RdynCoreTest, PoseComputation) {
   Eigen::VectorXd q(n_joints);
-  q.setZero();
-  Eigen::VectorXd Dq(n_joints);
-  Dq.setZero();
-  Eigen::VectorXd DDq(n_joints);
-  DDq.setZero();
-  Eigen::VectorXd DDDq(n_joints);
-  DDDq.setZero();
-  Eigen::VectorXd tau(n_joints);
-  tau.setZero();
+  q.setRandom();
+  Eigen::Affine3d T_base_tool = chain->getTransformation(q);
+  ASSERT_TRUE(T_base_tool.matrix().allFinite());
+}
 
-
-
-  Eigen::Affine3d T_base_tool;
-
-  Eigen::Vector6d twist_of_tool_in_base;
-  Eigen::Vector6d nonlinacc_twist_of_tool_in_base;
-  Eigen::Vector6d linacc_twist_of_tool_in_base;
-  Eigen::Vector6d acc_twist_of_tool_in_base;
-  Eigen::Vector6d jerk_twist_of_tool_in_base;
-
-  std::vector< Eigen::Vector6d, Eigen::aligned_allocator<Eigen::Vector6d> > twists, nonlinacc_twists, linacc_twists, acc_twists, jerk_twists;  // NOLINT(whitespace/line_length)
-
+TEST_F(RdynCoreTest, JacobianComputation) {
+  Eigen::VectorXd q(n_joints);
+  q.setRandom();
   Eigen::Matrix6Xd jacobian_of_tool_in_base;
-  jacobian_of_tool_in_base.resize(6, chain->getActiveJointsNumber());
-  Eigen::MatrixXd joint_inertia;
+  jacobian_of_tool_in_base.resize(6, n_joints);
+  jacobian_of_tool_in_base = chain->getJacobian(q);
+  ASSERT_TRUE(jacobian_of_tool_in_base.allFinite());
+}
 
-  double t_null = 0;
+TEST_F(RdynCoreTest, TwistComputation) {
+  Eigen::VectorXd q(n_joints);
+  Eigen::VectorXd Dq(n_joints);
+  q.setRandom();
+  Dq.setRandom();
+  std::vector<Eigen::Vector6d, Eigen::aligned_allocator<Eigen::Vector6d>> twists;
+  twists = chain->getTwist(q, Dq);
+  for (const auto& twist : twists) {
+    ASSERT_TRUE(twist.allFinite());
+  }
+}
+
+TEST_F(RdynCoreTest, LinearAccelerationTwistComputation) {
+  Eigen::VectorXd q(n_joints);
+  Eigen::VectorXd DDq(n_joints);
+  q.setRandom();
+  DDq.setRandom();
+  std::vector<Eigen::Vector6d, Eigen::aligned_allocator<Eigen::Vector6d>> linacc_twists;
+  linacc_twists = chain->getDTwistLinearPart(q, DDq);
+  for (const auto& linacc_twist : linacc_twists) {
+    ASSERT_TRUE(linacc_twist.allFinite());
+  }
+}
+
+TEST_F(RdynCoreTest, NonLinearAccelerationTwistComputation) {
+  Eigen::VectorXd q(n_joints);
+  Eigen::VectorXd Dq(n_joints);
+  q.setRandom();
+  Dq.setRandom();
+  std::vector<Eigen::Vector6d, Eigen::aligned_allocator<Eigen::Vector6d>> nonlinacc_twists;
+  nonlinacc_twists = chain->getDTwistNonLinearPart(q, Dq);
+  for (const auto& nonlinacc_twist : nonlinacc_twists) {
+    ASSERT_TRUE(nonlinacc_twist.allFinite());
+  }
+}
+
+TEST_F(RdynCoreTest, AccelerationTwistComputation) {
+  Eigen::VectorXd q(n_joints);
+  Eigen::VectorXd Dq(n_joints);
+  Eigen::VectorXd DDq(n_joints);
+  q.setRandom();
+  Dq.setRandom();
+  DDq.setRandom();
+  std::vector<Eigen::Vector6d, Eigen::aligned_allocator<Eigen::Vector6d>> acc_twists;
+  acc_twists = chain->getDTwist(q, Dq, DDq);
+  for (const auto& acc_twist : acc_twists) {
+    ASSERT_TRUE(acc_twist.allFinite());
+  }
+}
+
+TEST_F(RdynCoreTest, JerkTwistComputation) {
+  Eigen::VectorXd q(n_joints);
+  Eigen::VectorXd Dq(n_joints);
+  Eigen::VectorXd DDq(n_joints);
+  Eigen::VectorXd DDDq(n_joints);
+  q.setRandom();
+  Dq.setRandom();
+  DDq.setRandom();
+  DDDq.setRandom();
+  std::vector<Eigen::Vector6d, Eigen::aligned_allocator<Eigen::Vector6d>> jerk_twists;
+  jerk_twists = chain->getDDTwist(q, Dq, DDq, DDDq);
+  for (const auto& jerk_twist : jerk_twists) {
+    ASSERT_TRUE(jerk_twist.allFinite());
+  }
+}
+
+TEST_F(RdynCoreTest, JointTorqueComputation) {
+  Eigen::VectorXd q(n_joints);
+  Eigen::VectorXd Dq(n_joints);
+  Eigen::VectorXd DDq(n_joints);
+  q.setRandom();
+  Dq.setRandom();
+  DDq.setRandom();
+  Eigen::VectorXd tau = chain->getJointTorque(q, Dq, DDq);
+  ASSERT_TRUE(tau.allFinite());
+}
+
+TEST_F(RdynCoreTest, JointInertiaComputation) {
+  Eigen::VectorXd q(n_joints);
+  q.setRandom();
+  Eigen::MatrixXd joint_inertia = chain->getJointInertia(q);
+  ASSERT_TRUE(joint_inertia.allFinite());
+}
+
+TEST_F(RdynCoreTest, ComputationTime) {
+  Eigen::VectorXd q(n_joints);
+  Eigen::VectorXd Dq(n_joints);
+  Eigen::VectorXd DDq(n_joints);
+  Eigen::VectorXd DDDq(n_joints);
+
   double t_pose_eigen = 0;
   double t_jac_eigen = 0;
   double t_vel_eigen = 0;
-  double t_acc_eigen = 0;
-  double t_jerk_eigen = 0;
   double t_linacc_eigen = 0;
   double t_nonlinacc_eigen = 0;
+  double t_acc_eigen = 0;
+  double t_jerk_eigen = 0;
   double t_torque_eigen = 0;
   double t_inertia_eigen = 0;
-  auto t0 = std::chrono::high_resolution_clock::now();
-  int ntrial = 1e4;
+  int ntrial = 1e6;
 
-
-  for (int idx = 0; idx < ntrial; idx++)
-  {
+  for (int idx = 0; idx < ntrial; idx++) {
     q.setRandom();
     Dq.setRandom();
     DDq.setRandom();
     DDDq.setRandom();
 
-    t0 = std::chrono::high_resolution_clock::now();
-    t_null += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count() ;
-    t0 = std::chrono::high_resolution_clock::now();
+    auto t0 = std::chrono::high_resolution_clock::now();
+    chain->getTransformation(q);
+    t_pose_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count();
 
-    T_base_tool = chain->getTransformation(q);
-    t_pose_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count() ;
-
-    q.setRandom();
-    Dq.setRandom();
-    DDq.setRandom();
-    DDDq.setRandom();
     t0 = std::chrono::high_resolution_clock::now();
-    jacobian_of_tool_in_base = chain->getJacobian(q);
-    t_jac_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count() ;
+    chain->getJacobian(q);
+    t_jac_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count();
 
-    q.setRandom();
-    Dq.setRandom();
-    DDq.setRandom();
-    DDDq.setRandom();
     t0 = std::chrono::high_resolution_clock::now();
-    twists = chain->getTwist(q, Dq);
-    t_vel_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count() ;
+    chain->getTwist(q, Dq);
+    t_vel_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count();
 
-    q.setRandom();
-    Dq.setRandom();
-    DDq.setRandom();
-    DDDq.setRandom();
     t0 = std::chrono::high_resolution_clock::now();
-    linacc_twists = chain->getDTwistLinearPart(q, DDq);
-    t_linacc_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count() ;
+    chain->getDTwistLinearPart(q, DDq);
+    t_linacc_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count();
 
-    q.setRandom();
-    Dq.setRandom();
-    DDq.setRandom();
-    DDDq.setRandom();
     t0 = std::chrono::high_resolution_clock::now();
-    nonlinacc_twists = chain->getDTwistNonLinearPart(q, Dq);
-    t_nonlinacc_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count() ;
+    chain->getDTwistNonLinearPart(q, Dq);
+    t_nonlinacc_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count();
 
-    q.setRandom();
-    Dq.setRandom();
-    DDq.setRandom();
-    DDDq.setRandom();
     t0 = std::chrono::high_resolution_clock::now();
-    acc_twists = chain->getDTwist(q, Dq, DDq);
-    t_acc_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count() ;
+    chain->getDTwist(q, Dq, DDq);
+    t_acc_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count();
 
-    q.setRandom();
-    Dq.setRandom();
-    DDq.setRandom();
-    DDDq.setRandom();
     t0 = std::chrono::high_resolution_clock::now();
-    jerk_twists = chain->getDDTwist(q, Dq, DDq, DDDq);
-    t_jerk_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count() ;
+    chain->getDDTwist(q, Dq, DDq, DDDq);
+    t_jerk_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count();
 
-    q.setRandom();
-    Dq.setRandom();
-    DDq.setRandom();
-    DDDq.setRandom();
     t0 = std::chrono::high_resolution_clock::now();
-    tau = chain->getJointTorque(q, Dq, DDq);
-    t_torque_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count() ;
+    chain->getJointTorque(q, Dq, DDq);
+    t_torque_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count();
 
-    q.setRandom();
-    Dq.setRandom();
-    DDq.setRandom();
-    DDDq.setRandom();
     t0 = std::chrono::high_resolution_clock::now();
-    joint_inertia = chain->getJointInertia(q);
-    t_inertia_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count() ;
-
-    twist_of_tool_in_base = chain->getTwistTool(q, Dq);
-    linacc_twist_of_tool_in_base = chain->getDTwistLinearPartTool(q, DDq);
-    nonlinacc_twist_of_tool_in_base = chain->getDTwistNonLinearPartTool(q, Dq);
-    acc_twist_of_tool_in_base = chain->getDTwistTool(q, Dq, DDq);
-    jerk_twist_of_tool_in_base = chain->getDDTwistTool(q, Dq, DDq, DDDq);
+    chain->getJointInertia(q);
+    t_inertia_eigen += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - t0).count();
   }
 
-  printf("average on %d trials: \nnote:\ncompute torque implies computing acceleration,\ncompute acceleration implies computing velocity,\ncompute velocity implies computing pose\n", ntrial);  // NOLINT(whitespace/line_length)
-  printf("computation time No operation                                    = %8.5f [us]\n", t_null / (double)ntrial);
-  printf("computation time pose                                            = %8.5f [us]\n", t_pose_eigen / ntrial);
-  printf("computation time jacobian                                        = %8.5f [us]\n", t_jac_eigen / ntrial);
-  printf("computation time velocity twists for all links                   = %8.5f [us]\n", t_vel_eigen / ntrial);
-  printf("computation time linear acceleration twists for all links        = %8.5f [us]\n", t_linacc_eigen / ntrial);
-  printf("computation time non linear acceleration twists for all links    = %8.5f [us]\n", t_nonlinacc_eigen / ntrial);
-  printf("computation time acceleration twists for all links               = %8.5f [us]\n", t_acc_eigen  / ntrial);
-  printf("computation time jerk twists for all links                       = %8.5f [us]\n", t_jerk_eigen / ntrial);
-  printf("computation time joint torque                                    = %8.5f [us]\n", t_torque_eigen / ntrial);
-  printf("computation time joint inertia                                   = %8.5f [us]\n", t_inertia_eigen / ntrial);
+  printf("Average on %d trials:\n", ntrial);
+  printf("Computation time pose                                            = %8.5f [us]\n", t_pose_eigen / ntrial);
+  printf("Computation time jacobian                                        = %8.5f [us]\n", t_jac_eigen / ntrial);
+  printf("Computation time velocity twists for all links                   = %8.5f [us]\n", t_vel_eigen / ntrial);
+  printf("Computation time linear acceleration twists for all links        = %8.5f [us]\n", t_linacc_eigen / ntrial);
+  printf("Computation time non linear acceleration twists for all links    = %8.5f [us]\n", t_nonlinacc_eigen / ntrial);
+  printf("Computation time acceleration twists for all links               = %8.5f [us]\n", t_acc_eigen / ntrial);
+  printf("Computation time jerk twists for all links                       = %8.5f [us]\n", t_jerk_eigen / ntrial);
+  printf("Computation time joint torque                                    = %8.5f [us]\n", t_torque_eigen / ntrial);
+  printf("Computation time joint inertia                                   = %8.5f [us]\n", t_inertia_eigen / ntrial);
+}
 
-  auto st = std::chrono::high_resolution_clock::now();
-  std::thread::id this_id = std::this_thread::get_id();
-  auto et = std::chrono::high_resolution_clock::now();
-  std::cout << "GET ID "  << this_id << " | TIME  = " << std::chrono::duration_cast<std::chrono::microseconds>(et-st).count() << "[us]" << std::endl;
-  return 0;
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
